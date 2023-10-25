274ed5c98b2cd441ff0877e5dacb67f408ea52f7
ThatDawgMeta <121377253+ThatDawgMeta@users.noreply.github.com>
2023-10-25 09:41:29 -0500

    Updated to V1.1
    Include option to restart at the end and removed extraneous unused code.

--- Snake/Snek.cpp
+++ Snake/Snek.cpp
@@ -1,9 +1,9 @@
 /*
-				    |||||       ||||    ||		|||||||		||  ||
+					|||||       ||||    ||		|||||||		||  ||
 				   |||			|| ||   ||      ||			|| ||
-				    ||||		||  ||  ||		|||||||		||||
-				      |||		||   || ||		||			|| ||
-	 			  ||||||		||    ||||		|||||||		||  ||				*By MetaCerberus & MemoryScan
+					||||		||  ||  ||		|||||||		||||
+					  |||		||   || ||		||			|| ||
+				  ||||||		||    ||||		|||||||		||  ||				*By MetaCerberus & MemoryScan
 */
 
 
@@ -32,11 +32,10 @@ int yMax = 0;
 int xMax = 0;
 int movingDir = RIGHT; //remember character pressed
 bool dead = false;
-int buffer [120][30]; //Need to fix later to remove magic numbers.
+int buffer[120][30]; //Need to fix later to remove magic numbers.
 int score = 0;
 const int snakeMaxLength = 100;
 queue<COORD> snakeBody;
-int snakeLength = 1;
 uint16_t speedDelay = 200;
 int highScore = 0;
 
@@ -77,7 +76,7 @@ void snakeStats()
 
 //Moves snake automatically
 void moveSnake(int dir) {
-	if (dir == UP && movingDir == DOWN) 
+	if (dir == UP && movingDir == DOWN)
 	{
 		return;
 	}
@@ -158,7 +157,7 @@ void moveSnake(int dir) {
 		gotoxy(poppedElement.X, poppedElement.Y);
 		cout << ' ';
 		buffer[poppedElement.X][poppedElement.Y] = 0;
-		
+
 	}
 	else
 	{
@@ -173,153 +172,168 @@ void moveSnake(int dir) {
 
 int main()
 {
-	hideCursor();
-
-
-	//Define miliseconds
-	uint64_t msMove = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-	uint64_t updateMove = msMove + 100;
-	uint64_t secGen = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
-	uint64_t updateGen = secGen + 1;
-
-
-	//Getting Console Size
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	yMax = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-	xMax = csbi.dwSize.X;
-	//cout << "X Size is " << xMax << endl;  //Display X Size
-	//cout << "Y Size is " << yMax << endl;  //Display Y Size
-	for (int i = 0; i < xMax; i++)
-	{
-		for (int j = 0; j < yMax; j++)
-		{
-			buffer[i][j] = 0;
-		}
-	}
-	//Display Top Border and Scoring
-	scoreUpdate();
-	gotoxy((xMax / 2) - 2, 0);
-	cout << "SNEK";
-	gotoxy((xMax / 2) - 14, 1);
-	cout << "By MetaCerberus & MemoryScan";
-	gotoxy(0, 2);
-	for (int i = 0; i < xMax; i++)
-	{
-		cout << '_';
-	}
-	const string SCORE_SHEET = "scoresheet.txt";
-	ifstream scoreSheet;
-	scoreSheet.open(SCORE_SHEET);
-	if (scoreSheet.good())
-	{
-		scoreSheet >> highScore;
-	}
-	gotoxy(90, 1);
-	cout << "High Score: " << highScore;
-	scoreSheet.close();
-
-	//Set cursor position on screen
-	posX = xMax / 2;
-	posY = yMax / 2;
-	gotoxy(posX, posY);
-	
-	//Set initial character on screen
-	cout << 'O';
-	gotoxy(posX, posY);
-	snakeBody.push(COORD{ (short)posX, (short)posY });
-
-
-	//Allow for non blocking keyboard input
-	while (true) {
-		//Placing Food
-		uint64_t secGen = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
-
-		//Random Number Generator For Food
-		random_device FoodGen;
-		uniform_int_distribution<> xFoodPos(0, xMax - 1);
-		uniform_int_distribution<> yFoodPos(3, yMax - 1);
-		
-		//Place Food
-		int xFoodPlace = xFoodPos(FoodGen);
-		int yFoodPlace = yFoodPos(FoodGen);
-		if (secGen > updateGen)
-		{
-			updateGen = secGen + 6;
-			if (buffer[xFoodPlace][yFoodPlace] != 2)
-			{
-				gotoxy(xFoodPlace, yFoodPlace);
-				cout << 'X';
-				gotoxy(posX, posY);
-				buffer[xFoodPlace][yFoodPlace] = 1;
-			}
-		}
-
-		msMove = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-		if (msMove > updateMove) 
-		{
-			updateMove = msMove + speedDelay;
-			moveSnake(movingDir);
-		}
-		if (_kbhit()) { // Check if a key has been pressed
-			char key = _getch(); // Read the pressed key
-			if (key == 27) { // Check if the Esc key (ASCII 27) was pressed
-				break; // Exit the loop if the Esc key is pressed
-			}
-			//Move character up
-			else if(key == 119) {
-				moveSnake(UP);
-			}
-			//move character down
-			else if (key == 115) {
-				moveSnake(DOWN);
-			}
-			//move character left
-			else if (key == 97) {
-				moveSnake(LEFT);
-			}
-			//move character right
-			else if (key == 100) {
-				moveSnake(RIGHT);
-			}
-		}
-		if (dead)
-		{
-			break;
-		}
-	}
-	//recording high score
-	if (highScore < score)
-	{
-		highScore = score;
-		ofstream scoreSheet;
-		scoreSheet.open(SCORE_SHEET);
-		scoreSheet << highScore;
-		scoreSheet.close();
-	}
-	
-	gotoxy(xMax / 2 - 14, yMax / 2 );
-	cout << "    Press Escape To Exit    ";
 	while (true)
 	{
-		if (_kbhit())
+		dead = false;
+		score = 0;
+		snakeBody = {};
+		hideCursor();
+
+
+		//Define miliseconds
+		uint64_t msMove = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+		uint64_t updateMove = msMove + 100;
+		uint64_t secGen = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
+		uint64_t updateGen = secGen + 1;
+
+
+		//Getting Console Size
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+		yMax = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+		xMax = csbi.dwSize.X;
+		//cout << "X Size is " << xMax << endl;  //Display X Size
+		//cout << "Y Size is " << yMax << endl;  //Display Y Size
+		for (int i = 0; i < xMax; i++)
 		{
-			char key = _getch(); // Read the pressed key
-			if (key == 27) { // Check if the Esc key (ASCII 27) was pressed
-				break; // Exit the loop if the Esc key is pressed
+			for (int j = 0; j < yMax; j++)
+			{
+				buffer[i][j] = 0;
+				gotoxy(i, j);
+				cout << ' ';
 			}
 		}
+		//Display Top Border and Scoring
+		scoreUpdate();
+		gotoxy((xMax / 2) - 2, 0);
+		cout << "SNEK";
+		gotoxy((xMax / 2) - 14, 1);
+		cout << "By MetaCerberus & MemoryScan";
+		gotoxy(0, 2);
+		for (int i = 0; i < xMax; i++)
+		{
+			cout << '_';
+		}
+		const string SCORE_SHEET = "scoresheet.txt";
+		ifstream scoreSheet;
+		scoreSheet.open(SCORE_SHEET);
+		if (scoreSheet.good())
+		{
+			scoreSheet >> highScore;
+		}
+		gotoxy(90, 1);
+		cout << "High Score: " << highScore;
+		scoreSheet.close();
+
+		//Set cursor position on screen
+		posX = xMax / 2;
+		posY = yMax / 2;
+		gotoxy(posX, posY);
+
+		//Set initial character on screen
+		cout << 'O';
+		gotoxy(posX, posY);
+		snakeBody.push(COORD{ (short)posX, (short)posY });
+
+
+		//Allow for non blocking keyboard input
+		while (true) {
+			//Placing Food
+			uint64_t secGen = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
+
+			//Random Number Generator For Food
+			random_device FoodGen;
+			uniform_int_distribution<> xFoodPos(0, xMax - 1);
+			uniform_int_distribution<> yFoodPos(3, yMax - 1);
+
+			//Place Food
+			int xFoodPlace = xFoodPos(FoodGen);
+			int yFoodPlace = yFoodPos(FoodGen);
+			if (secGen > updateGen)
+			{
+				updateGen = secGen + 6;
+				if (buffer[xFoodPlace][yFoodPlace] != 2)
+				{
+					gotoxy(xFoodPlace, yFoodPlace);
+					cout << 'X';
+					gotoxy(posX, posY);
+					buffer[xFoodPlace][yFoodPlace] = 1;
+				}
+			}
+
+			msMove = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+			if (msMove > updateMove)
+			{
+				updateMove = msMove + speedDelay;
+				moveSnake(movingDir);
+			}
+			if (_kbhit()) { // Check if a key has been pressed
+				char key = _getch(); // Read the pressed key
+				if (key == 27) { // Check if the Esc key (ASCII 27) was pressed
+					break; // Exit the loop if the Esc key is pressed
+				}
+				//Move character up
+				else if (key == 119) {
+					moveSnake(UP);
+				}
+				//move character down
+				else if (key == 115) {
+					moveSnake(DOWN);
+				}
+				//move character left
+				else if (key == 97) {
+					moveSnake(LEFT);
+				}
+				//move character right
+				else if (key == 100) {
+					moveSnake(RIGHT);
+				}
+			}
+			if (dead)
+			{
+				break;
+			}
+		}
+		//recording high score
+		if (highScore < score)
+		{
+			highScore = score;
+			ofstream scoreSheet;
+			scoreSheet.open(SCORE_SHEET);
+			scoreSheet << highScore;
+			scoreSheet.close();
+		}
+
+		gotoxy(xMax / 2 - 22, yMax / 2);
+		cout << "    Press Escape To Exit Or 1 To Restart    ";
+		while (true)
+		{
+			if (_kbhit())
+			{
+				char key = _getch(); // Read the pressed key
+				if (key == 27) { // Check if the Esc key (ASCII 27) was pressed
+					exit(0); // Exit the game if the Esc key is pressed
+				}
+				else if (key == 49) // 1 key
+				{
+					gotoxy(xMax / 2 - 5, yMax / 2 + 1);
+					cout << "Restarting";
+					break;
+				}
+
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
 	}
-
-
-
-
-	
-
-
-
-
-
-
-
 }
